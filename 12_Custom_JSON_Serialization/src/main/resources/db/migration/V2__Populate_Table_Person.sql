d115a12f745b2bd3919e6e5bd90612451cb9e9fe
Gust4ngl <gustavosilvaskt@outlook.com>
2022-12-27 12:03:20 -0300

    12_Custom_JSON_Serialization

--- 12_Custom_JSON_Serialization/src/main/resources/db/migration/V2__Populate_Table_Person.sql
+++ 12_Custom_JSON_Serialization/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -1,4 +1,4 @@
-INSERT INTO `person` VALUES
+INSERT INTO `person` (id, address, first_name, gender, last_name) VALUES
 	(2,'BR0','Carlos','M','Pinauba'),
 	(3,'BR0','Jão','M','Pinauba'),
 	(6,'EUA','aaa','F','cearense');
